eaa0e1b6cdb5fbcbdb697385368c98ce6074e06c
Oliver Samuels <osamuels@bu.edu>
2023-07-27 12:19:44 -0400

    App name change

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have as title 'client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('Fitness Center App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Fitness Center app is running!');
   });
 });
