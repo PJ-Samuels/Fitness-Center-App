ef60f0076db5c12b6fff469ec70b82cd8795d4f2
Oliver Samuels <osamuels@bu.edu>
2023-10-20 18:05:31 -0400

    angular component integration

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -25,6 +25,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input'
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 
