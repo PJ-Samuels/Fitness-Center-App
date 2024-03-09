ecd9cbe916c68d4ebcac18dbc329d1098c93eaf6
Oliver Samuels <osamuels@bu.edu>
2024-03-08 21:59:08 -0500

    fitness finder

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,12 +1,21 @@
+const projectId = process.env['REACT_APP_PROJECT_ID'];
+const appId = process.env['REACT_APP_APP_ID'];
+const storageBucket = process.env['REACT_APP_STORAGE_BUCKET'];
+const apiKey = process.env['REACT_APP_API_KEY'];
+const authDomain = process.env['REACT_APP_AUTH_DOMAIN'];
+const messagingSenderId = process.env['REACT_APP_MESSAGING_SENDER_ID'];
+const measurementId = process.env['REACT_APP_MEASUREMENT_ID'];
+
+
 export const environment = {
   firebase: {
-    projectId: 'fitness-center-app-5de19',
-    appId: '1:815520523732:web:de84fb1eb64bcae6672241',
-    storageBucket: 'fitness-center-app-5de19.appspot.com',
-    apiKey: 'AIzaSyCRpVG83AHFTE9kLeY3lZyOJlY8HzQPbVk',
-    authDomain: 'fitness-center-app-5de19.firebaseapp.com',
-    messagingSenderId: '815520523732',
-    measurementId: 'G-82R7V0V9D3',
+    projectId: projectId,
+    appId: appId,
+    storageBucket: storageBucket,
+    apiKey: apiKey,
+    authDomain: authDomain,
+    messagingSenderId: messagingSenderId,
+    measurementId: measurementId,
   },
   production: false,
 };
